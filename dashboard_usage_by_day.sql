21ffa082fb8839dc4b029a322db3805a7a8d03ae
ALVARO MARTINS ALVES <84479495+AlvaroMAlves@users.noreply.github.com>
2023-08-25 08:12:14 -0300

    Update dashboard_usage_by_day.sql
    Some underlines are meant to be points!

--- dashboard_usage_by_day.sql
+++ dashboard_usage_by_day.sql
@@ -2,8 +2,8 @@ select
   to_char(public.view_log.timestamp + interval + '2 hours', 'yyyy_mm_dd') as event_at,
   count(*)
 from public.view_log
-inner join public.report_dashboard on (public.report_dashboard.id = public.view_log_model_id)
-inner join public.core_user on (public.view_log_user_id = public.core_user_id) 
+inner join public.report_dashboard on (public.report_dashboard.id = public.view_log.model_id)
+inner join public.core_user on (public.view_log.user_id = public.core_user.id) 
 where public.view_log.model = 'dashboard'
 and public.view_log.timestamp > current_date - 90
 group by 1
